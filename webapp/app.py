250a67527e67cc9363536f7901a2058910708679
arubioru <163845505+arubioru@users.noreply.github.com>
2024-03-18 13:54:52 +0100

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'C'est quoi ce poulet!'
+    return 'Hello World Antoine!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
